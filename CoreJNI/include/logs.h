c29c62d1d3877a2be00b988e168b3b4408db49cf
李樟取 <lizhangqu@weidian.com>
2017-07-31 22:17:28 +0800

    update for release

--- CoreJNI/include/logs.h
+++ CoreJNI/include/logs.h
@@ -1,12 +1,23 @@
 #ifndef CORE_LOG_H
 #define CORE_LOG_H
 
+#ifdef NDEBUG
+#define LOGV(...)
+#define LOGD(...)
+#define LOGI(...)
+#define LOGW(...)
+#define LOGE(...)
+
+#else
+
 #ifndef CORE_LOG_TAG
 #define CORE_LOG_TAG "CoreJNI"
 #endif
 
 #ifdef ANDROID
+
 #include <android/log.h>
+
 #define LOGV(fmt, ...) __android_log_print(ANDROID_LOG_VERBOSE, CORE_LOG_TAG, fmt, ##__VA_ARGS__)
 #define LOGD(fmt, ...) __android_log_print(ANDROID_LOG_DEBUG, CORE_LOG_TAG, fmt, ##__VA_ARGS__)
 #define LOGI(fmt, ...) __android_log_print(ANDROID_LOG_INFO, CORE_LOG_TAG, fmt, ##__VA_ARGS__)
@@ -21,6 +32,8 @@
 #define LOGI(...) printf("< %s  >\t<%s>\t","info", CORE_LOG_TAG); printf(__VA_ARGS__); printf("\n");
 #define LOGW(...) printf("<%s>\t<%s>\t","warning", CORE_LOG_TAG); printf(__VA_ARGS__); printf("\n");
 #define LOGE(...) printf("< %s >\t<%s>\t","error", CORE_LOG_TAG); printf(__VA_ARGS__); printf("\n");
+#endif
+
 #endif
 
 #endif //CORE_LOG_H
